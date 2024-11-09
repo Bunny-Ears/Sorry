944e3a6f8786f99238c10e70502db964dfe4ffa5
Bunny-Ears <tourney-07tripod@icloud.com>
2024-11-09 20:00:29 +0000

    update

---
+++ script.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector('button');
+const money = document.querySelector('h2');
+const presses = document.querySelector('h3');
+let value = 0;
+let times = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM fully loaded and parsed");
+    value = localStorage.getItem('value') || 0;
+    times = localStorage.getItem('times') || 0;
+    console.log(value);
+    value = JSON.parse(value);
+    times = JSON.parse(times);
+    console.log(times, value)
+    money.innerText = `£${value}`;
+    presses.innerText = `${times} sorrys`;
+});
+
+
+btn.onclick = () => {
+    value += 0.10;
+    times++;
+    round = value.toFixed(2);
+    localStorage.setItem('value',round);
+    localStorage.setItem('times', times);
+    money.innerText = `£${round}`;
+    presses.innerText = `${times} sorrys`;
+
+};
+
